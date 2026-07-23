66fc51275b9b54461ebb94a4f044fe04b3cc0c9a
iammuntakim <mdmuntakimamin@gmail.com>
2026-07-23 11:12:27 +0600

    Build SuperSU

--- app/core/build.gradle.kts
+++ app/core/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 }
 
 dependencies {
+    implementation(files("lib/native.jar"))
+
     api(project(":shared"))
     coreLibraryDesugaring(libs.jdk.libs)
 
@@ -63,9 +65,6 @@ dependencies {
     implementation(libs.collection.ktx)
     implementation(libs.profileinstaller)
 
-    // We also implement all our tests in this module.
-    // However, we don't want to bundle test dependencies.
-    // That's why we make it compileOnly.
     compileOnly(libs.test.junit)
     compileOnly(libs.test.uiautomator)
 }
