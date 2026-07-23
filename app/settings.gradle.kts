d4d5de1c6f5397cd95587bbb0d46087dd4c3bb5f
iammuntakim <mdmuntakimamin@gmail.com>
2026-07-23 11:29:37 +0600

    Build SuperSU

--- app/settings.gradle.kts
+++ app/settings.gradle.kts
@@ -5,7 +5,21 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://jitpack.io")
+        maven("https://maven.yukonga.top/releases")
     }
 }
+
+Plugins {
+    id("MagiskPlugin")
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+
+    subprojects.forEach {
+        dependsOn(":${it.name}:clean")
+    }
+}
+
 rootProject.name = "Magisk"
 include(":apk", ":core", ":shared", ":stub", ":test")
