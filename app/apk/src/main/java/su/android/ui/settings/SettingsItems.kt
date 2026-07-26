4f4984f94ff7f47a1cb58eb4e65e0fae3470ada9
iammuntakim <mdmuntakimamin@gmail.com>
2026-07-26 19:29:31 +0600

    Build SuperSU

--- app/apk/src/main/java/su/android/ui/settings/SettingsItems.kt
+++ app/apk/src/main/java/su/android/ui/settings/SettingsItems.kt
@@ -208,7 +208,7 @@ object Restrict : BaseSettingsItem.Toggle() {
 
 object Uninstall : BaseSettingsItem.Blank() {
     override val title = CoreR.string.uninstall.asText()
-    override fun onClick(context: Context) {
+    override fun onClick(context: Context, view: View) {
         UninstallDialog(context).show()
     }
 }
