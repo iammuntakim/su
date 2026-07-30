5c84a37f4dbdc22fc9ae736645c9ba0d6b295299
iammuntakim <mdmuntakimamin@gmail.com>
2026-07-30 13:38:29 +0600

    Build SuperSU

--- app/core/src/main/java/top/mauix/core/RecyclerView.java
+++ app/core/src/main/java/top/mauix/core/RecyclerView.java
@@ -25,24 +25,13 @@ public class RecyclerView extends androidx.recyclerview.widget.RecyclerView {
     public static abstract class OnScrollListener extends androidx.recyclerview.widget.RecyclerView.OnScrollListener {
         @Override
         public void onScrollStateChanged(@NonNull androidx.recyclerview.widget.RecyclerView recyclerView, int newState) {
-            if (recyclerView instanceof top.mauix.core.RecyclerView) {
-                onScrollStateChanged((top.mauix.core.RecyclerView) recyclerView, newState);
-            } else {
-                super.onScrollStateChanged(recyclerView, newState);
-            }
+            super.onScrollStateChanged(recyclerView, newState);
         }
 
         @Override
         public void onScrolled(@NonNull androidx.recyclerview.widget.RecyclerView recyclerView, int dx, int dy) {
-            if (recyclerView instanceof top.mauix.core.RecyclerView) {
-                onScrolled((top.mauix.core.RecyclerView) recyclerView, dx, dy);
-            } else {
-                super.onScrolled(recyclerView, dx, dy);
-            }
+            super.onScrolled(recyclerView, dx, dy);
         }
-
-        public void onScrollStateChanged(@NonNull top.mauix.core.RecyclerView recyclerView, int newState) {}
-        public void onScrolled(@NonNull top.mauix.core.RecyclerView recyclerView, int dx, int dy) {}
     }
 
     public static abstract class Adapter<VH extends androidx.recyclerview.widget.RecyclerView.ViewHolder> extends androidx.recyclerview.widget.RecyclerView.Adapter<VH> {}
