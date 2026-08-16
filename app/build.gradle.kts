b74926f7682e6aa1289e533a77b0942de4648a88
iammuntakim <mdmuntakimamin@gmail.com>
2026-08-16 19:24:17 +0600

    Add android block with compileSdk configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,3 +2,14 @@ plugins {
     id("SuperSuPlugin")
     id("com.android.application")
 }
+
+android {
+    compileSdk = release(36) {
+        minorApiLevel = 1
+    }
+    buildToolsVersion = "36.1.0"
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 36
+    }
+}
